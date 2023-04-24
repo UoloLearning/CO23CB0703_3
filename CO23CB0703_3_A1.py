d94560794081ba8fcc7d2235e7b6a0dfab620eaa
UoloLearning <127477826+UoloLearning@users.noreply.github.com>
2023-04-24 15:25:35 +0530

    Update CO23CB0703_3_A1.py

--- CO23CB0703_3_A1.py
+++ CO23CB0703_3_A1.py
@@ -1,1 +1,16 @@
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
 
+# Prompt the user to enter a number
+num = int(input("Enter a number: "))
+
+# Check if the number is prime
+if is_prime(num):
+    print(num, "is a prime number")
+else:
+    print(num, "is not a prime number")
