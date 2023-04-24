9d1fbf21f802563fa93834cc56423aedcdc26fd2
UoloLearning <127477826+UoloLearning@users.noreply.github.com>
2023-04-24 15:58:33 +0530

    Update CO23CB0703_3_A2.py

--- CO23CB0703_3_A2.py
+++ CO23CB0703_3_A2.py
@@ -1,1 +1,12 @@
+def square(num):
+    """This function calculates the square of a number."""
+    return num ** 2
 
+# Prompt the user to enter a number
+num = int(input("Enter a number: "))
+
+# Calculate the square of the number using the square function
+result = square(num)
+
+# Print the result
+print("The square of", num, "is", result)
