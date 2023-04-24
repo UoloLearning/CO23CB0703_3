a012b33b45ee85abd9feecd1977e22526f73dae3
UoloLearning <127477826+UoloLearning@users.noreply.github.com>
2023-04-24 16:20:13 +0530

    Update CO23CB0703_3_AA2.py

--- CO23CB0703_3_AA2.py
+++ CO23CB0703_3_AA2.py
@@ -1,1 +1,12 @@
+def calculation(a, b):
+    add_result = a + b
+    sub_result = a - b
+    return add_result, sub_result
 
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+addition, subtraction = calculation(num1, num2)
+
+print("The addition result is:", addition)
+print("The subtraction result is:", subtraction)
